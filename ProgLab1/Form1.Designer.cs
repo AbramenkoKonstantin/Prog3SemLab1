2af0a3aac8c50d0816ef2e6ca34493a7e046d9de
Константин Абраменко <kostaabramenko695@gmail.com>
2021-09-26 22:50:18 +0700

    First

---
+++ ProgLab1/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿
+namespace ProgLab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.textBoxA = new System.Windows.Forms.TextBox();
+            this.textBoxF = new System.Windows.Forms.TextBox();
+            this.textBoxAnswer = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxB = new System.Windows.Forms.TextBox();
+            this.textBoxE = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.funcChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.funcChart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxA
+            // 
+            this.textBoxA.Location = new System.Drawing.Point(39, 20);
+            this.textBoxA.Name = "textBoxA";
+            this.textBoxA.Size = new System.Drawing.Size(100, 20);
+            this.textBoxA.TabIndex = 0;
+            this.textBoxA.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Params_KeyPress);
+            // 
+            // textBoxF
+            // 
+            this.textBoxF.Location = new System.Drawing.Point(39, 149);
+            this.textBoxF.Name = "textBoxF";
+            this.textBoxF.Size = new System.Drawing.Size(127, 20);
+            this.textBoxF.TabIndex = 1;
+            // 
+            // textBoxAnswer
+            // 
+            this.textBoxAnswer.Enabled = false;
+            this.textBoxAnswer.Location = new System.Drawing.Point(39, 225);
+            this.textBoxAnswer.Name = "textBoxAnswer";
+            this.textBoxAnswer.Size = new System.Drawing.Size(127, 20);
+            this.textBoxAnswer.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 133);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Функция";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(78, 4);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "a";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Dock = System.Windows.Forms.DockStyle.None;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(25, 198);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(88, 24);
+            this.menuStrip1.TabIndex = 7;
+            this.menuStrip1.Text = "Рассчитать";
+            // 
+            // ToolStripMenuItem
+            // 
+            this.ToolStripMenuItem.Name = "ToolStripMenuItem";
+            this.ToolStripMenuItem.Size = new System.Drawing.Size(80, 20);
+            this.ToolStripMenuItem.Text = "Рассчитать";
+            this.ToolStripMenuItem.Click += new System.EventHandler(this.ToolStripMenuItem_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(78, 43);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(13, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "b";
+            // 
+            // textBoxB
+            // 
+            this.textBoxB.Location = new System.Drawing.Point(39, 59);
+            this.textBoxB.Name = "textBoxB";
+            this.textBoxB.Size = new System.Drawing.Size(100, 20);
+            this.textBoxB.TabIndex = 8;
+            this.textBoxB.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Params_KeyPress);
+            // 
+            // textBoxE
+            // 
+            this.textBoxE.Location = new System.Drawing.Point(39, 98);
+            this.textBoxE.Name = "textBoxE";
+            this.textBoxE.Size = new System.Drawing.Size(100, 20);
+            this.textBoxE.TabIndex = 10;
+            this.textBoxE.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Params_KeyPress);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(78, 82);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(13, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "e";
+            // 
+            // funcChart
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.funcChart.ChartAreas.Add(chartArea1);
+            this.funcChart.Location = new System.Drawing.Point(172, 4);
+            this.funcChart.Name = "funcChart";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Name = "Series1";
+            this.funcChart.Series.Add(series1);
+            this.funcChart.Size = new System.Drawing.Size(313, 241);
+            this.funcChart.TabIndex = 12;
+            this.funcChart.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(497, 260);
+            this.Controls.Add(this.funcChart);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxE);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxB);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxAnswer);
+            this.Controls.Add(this.textBoxF);
+            this.Controls.Add(this.textBoxA);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.funcChart)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxA;
+        private System.Windows.Forms.TextBox textBoxF;
+        private System.Windows.Forms.TextBox textBoxAnswer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem ToolStripMenuItem;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxB;
+        private System.Windows.Forms.TextBox textBoxE;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataVisualization.Charting.Chart funcChart;
+    }
+}
+
---
+++ ProgLab1/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using MathNet.Symbolics;
+using System.Windows.Forms;
+using System.Collections.Generic;
+
+namespace ProgLab1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double.TryParse(textBoxA.Text, out double aBord);
+                double.TryParse(textBoxB.Text, out double bBord);
+                double.TryParse(textBoxE.Text, out double esp);
+                Expression func = Infix.ParseOrThrow(textBoxF.Text);
+                if (esp < 0)
+                {
+                    throw new Exception();
+                }
+                int decimals = esp.ToString().Substring(esp.ToString().IndexOf(',')).Length - 1;
+                LineDraw(aBord, bBord, esp, func);
+                double minPoint = 0;
+                while (bBord - aBord >= esp)
+                {
+                    double mid = (aBord + bBord) / 2;
+                    double left = mid - esp;
+                    double right = mid + esp;
+                    if (FuncValue(left, func) < FuncValue(right, func))
+                    {
+                        bBord = mid;
+                    }
+                    else
+                    {
+                        aBord = mid;
+                    }
+                    minPoint = mid;
+                }
+                textBoxAnswer.Text = "(" + Math.Round(minPoint, decimals).ToString() + ":" + Math.Round(FuncValue(minPoint, func), decimals).ToString() + ")";
+            }
+            catch
+            {
+                if (textBoxA.Text == "" || textBoxB.Text == "" || textBoxE.Text == "" || textBoxF.Text == "")
+                {
+                    MessageBox.Show("Пустые поля недопустимы");
+                }
+                else if (double.Parse(textBoxE.Text) < 0)
+                {
+                    MessageBox.Show("Точность не может быть меньше 0");
+                }
+                else if (double.Parse(textBoxA.Text) >= double.Parse(textBoxB.Text))
+                {
+                    MessageBox.Show("Параметр a должен быть меньше параметра b");
+                }
+                else
+                {
+                    MessageBox.Show("Некорректно задана вычисляемая функция");
+                }
+            }
+        }
+        private void LineDraw(double aBord, double bBord, double esp, Expression func)
+        {
+            int decimals = esp.ToString().Substring(esp.ToString().IndexOf(',')).Length - 1;
+            funcChart.Series[0].Points.Clear();
+            while (aBord < bBord)
+            {
+                double funcValue = Math.Round(FuncValue(aBord, func), decimals);
+                funcChart.Series[0].Points.AddXY(aBord, funcValue);
+                aBord += esp;
+            }
+        }
+        private double FuncValue(double point, Expression func)
+        {
+            Dictionary<string, FloatingPoint> x = new Dictionary<string, FloatingPoint>()
+            {
+                { "x", point }
+            };
+            return Evaluate.Evaluate(x, func).RealValue;
+        }
+
+        private void Params_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            var textBox = (TextBox) sender;
+            if (char.IsDigit(e.KeyChar) || (e.KeyChar == ',' && textBox.Text.Contains(",") == false) || (e.KeyChar == '-' && textBox.Text.Contains("-") == false) || (e.KeyChar == (char)Keys.Back))
+            {
+                e.Handled = false;
+            }
+            else
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
